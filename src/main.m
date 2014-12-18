4c10f9bcf4636c1dd9721d4cb001c4d666c3bc1c
Hailey Somerville <hailey@hailey.lol>
2014-12-19 00:00:21 +1100

    ok this is sketchy as hell but it seems to make it work properly

--- src/main.m
+++ src/main.m
@@ -7,20 +7,21 @@ IVAR_DECL(NSAttributedString*, ABUITextRenderer, attributedString);
 IVAR_DECL(id, TMTimelineStatusCell, _fullNameRenderer);
 IVAR_DECL(id, TMTimelineStatusCell, _usernameRenderer);
 
-@interface TwitterPlus_TMTimelineStatusCell : NSObject
-- (void)original_prepareForDisplay;
-@end
-
-@implementation TwitterPlus_TMTimelineStatusCell
-- (void)prepareForDisplay
+static void
+swap_handle_and_full_name(id timeline_status_cell)
 {
-    [self original_prepareForDisplay];
-
-    id fullNameRenderer = TMTimelineStatusCell__fullNameRenderer_get(self);
-    id usernameRenderer = TMTimelineStatusCell__usernameRenderer_get(self);
+    id fullNameRenderer = TMTimelineStatusCell__fullNameRenderer_get(timeline_status_cell);
+    id usernameRenderer = TMTimelineStatusCell__usernameRenderer_get(timeline_status_cell);
 
     NSAttributedString* fullNameRendererStr = ABUITextRenderer_attributedString_get(fullNameRenderer);
     NSAttributedString* usernameRendererStr = ABUITextRenderer_attributedString_get(usernameRenderer);
+
+    // sketchy way to check if we've already swapped the full name and username
+    // yes, this will break if someone's full name starts with a '@'.
+    // patches welcome.
+    if([[fullNameRendererStr string] UTF8String][0] == '@') {
+        return;
+    }
 
     NSDictionary* fullNameRendererStrAttributes = [fullNameRendererStr attributesAtIndex:0 effectiveRange:NULL];
     NSDictionary* usernameRendererStrAttributes = [usernameRendererStr attributesAtIndex:0 effectiveRange:NULL];
@@ -36,6 +37,24 @@ IVAR_DECL(id, TMTimelineStatusCell, _usernameRenderer);
     [fullNameRendererStrAttributes release];
     [usernameRendererStrAttributes release];
 }
+
+@interface TwitterPlus_TMTimelineStatusCell : NSObject
+- (void)original_prepareForDisplay;
+- (BOOL)original_drawAsSpecial;
+@end
+
+@implementation TwitterPlus_TMTimelineStatusCell
+- (void)prepareForDisplay
+{
+    [self original_prepareForDisplay];
+    swap_handle_and_full_name(self);
+}
+
+- (BOOL)drawAsSpecial
+{
+    swap_handle_and_full_name(self);
+    return [self original_drawAsSpecial];
+}
 @end
 
 __attribute__((constructor)) void
@@ -46,6 +65,7 @@ flint_plus_main()
     IVAR_DEFN(TMTimelineStatusCell, _usernameRenderer);
 
     twitter_plus_patch("TMTimelineStatusCell", "prepareForDisplay");
+    twitter_plus_patch("TMTimelineStatusCell", "drawAsSpecial");
 
     fprintf(stderr, "TwitterPlus loaded!\n");
 }
